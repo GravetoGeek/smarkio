609949d3bdc01a6f8a241926040194dfae20e930
Eduardo Sa <edu_colisao69@hotmail.com>
2020-11-03 05:03:07 -0300

    Passo 04

---
+++ passo-04/AnaliseDescritiva.sql
@@ -0,0 +1,28 @@
+Status:
+
+select A.status,
+A.msg,
+A.msg/(select count(*) from mensagens)*100 as percentual
+from(
+select status.status,
+count(mensagens.idstatus) as "msg",
+(select count(*) from mensagens) as total
+from mensagens
+join status on mensagens.idstatus = status.idstatus
+group by mensagens.idstatus
+) as A
+
+
+
+Intenções:
+select A.nome,
+A.Mensagens,
+A.Mensagens/(select count(*) from mensagens)*100 as percentual
+from
+(select intencoes.nome,
+count(mensagens.idintencao) as "Mensagens"
+from mensagens
+join intencoes on mensagens.idintencao = intencoes.idintencao
+group by mensagens.idintencao
+) as A
+
